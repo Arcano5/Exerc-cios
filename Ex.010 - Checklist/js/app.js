425899aca48a31d9ef2db43019156a410c463c75
Leonildo Freitas Gomes <leonildo.f.gomes@gmail.com>
2025-08-05 19:38:43 -0300

    Segunda tentativa

---
+++ Ex.010 - Checklist/js/app.js
@@ -0,0 +1,11 @@
+const respostaPneus = document.querySelector('input[name = "pneusCalibrados"]:checked').value;
+const pneusCalibrados = respostaPneus === "true";
+
+const respostaLuzes = document.querySelector('input[name = "luzFuncionando"]:checked').value;
+const luzFuncionando = respostaLuzes === "true";
+
+const respostaOa = document.querySelector('input[name = "oaVerificados"]:checked').value;
+const oaVerificados = respostaOa === "true";
+
+const respostaDoc = document.querySelector('input[name = "docVerificado"]:checked').value;
+const docVerificado = respostaDoc === "true";
